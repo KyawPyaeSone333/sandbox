8fe0c4ee4df690c1970462156624209d4cc54b28
kSoLong <kyawpyaesone@my.jcu.edu.au>
2019-08-04 19:02:56 +0800

    add name printing program

--- oddname.py
+++ oddname.py
@@ -1,3 +1,12 @@
 """
 KyawPyaeSone
 """
+
+username = input("Please enter your name: ")
+
+if " " in username:
+    print(" ** Should not contain spaces ** ")
+    username = input("Please enter your name : ")
+    print(username[1])
+else:
+    print(username[1])
